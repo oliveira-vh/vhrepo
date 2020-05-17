dbfcd00eb398046fba895798bfccb67ad042c05f
Victor de Oliveira <59441152+oliveira-vh@users.noreply.github.com>
2020-05-17 12:14:18 -0300

    Create fetch-weather-celsius.js

---
+++ fetch-weather-celsius.js
@@ -0,0 +1,28 @@
+const fetch = require("node-fetch"); //needs node-fetch dependency 
+const url = 'https://api.openweathermap.org/data/2.5/weather?q='
+const apiKey = '' //openweather api key
+
+const city = 'Belem'
+
+async function getData() {
+	try {
+		const response = await fetch(`${url}${city}&appid=${apiKey}`)
+
+		if (response.ok) {
+			const jsonResponse = await response.json()
+			//console.log(response)
+			//console.log(jsonResponse.main.temp)
+			//console.log(jsonResponse.weather)
+			const temp_city_celsius = jsonResponse.main.temp - 273.15
+			console.log(temp_city_celsius)
+
+		} else {
+			throw new Error('Deu Erro!')
+		} 
+
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+getData()
